ed8c1ea3b2bfa2a0bd431b85fe27b687eaab3e8e
Coolshanlan <joey0201020123@gmail.com>
2020-02-11 21:59:06 +0800

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Q_Learning
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(363, 363);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Q_Learning";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Georgia", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(437, 32);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(126, 36);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Start Learning";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Georgia", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(437, 89);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 36);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Stop Learning";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(404, 182);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(183, 189);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Step";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 19);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(171, 164);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(437, 131);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Minimum = 10;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(126, 45);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.Value = 10;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(581, 131);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 12);
+            this.label1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 383);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Q_Learning
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        double[,,] Qtable = new double[5,5,4];
+        Button[,] bu = new Button[5,5];
+        void addbutton()
+        {
+            bu = new Button[5, 5];
+            for(int x = 0; x < 5; x++)
+            {
+                for (int y = 0; y < 5; y++)
+                {
+                    bu[x, y] = new Button();
+                    bu[x, y].Text = "";
+                    bu[x, y].Location = new Point(20 + x * 66, 20 + y * 66);
+                    bu[x, y].Size = new Size(65,65);
+                    bu[x, y].BackColor = System.Drawing.Color.White;
+                    bu[x, y].FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+                    groupBox1.Controls.Add(bu[x, y]);
+                }
+            }
+        }
+        List<Point> treasure = new List<Point>()
+        {
+            new Point(2,0)
+        };
+        List<Point> trap = new List<Point>()
+        {
+            new Point(1,0),
+            new Point(1,1),
+            new Point(1,2),
+            new Point(2,3),
+            new Point(4,3),
+            // new Point(2,3),
+            //  new Point(3,3),
+            //   new Point(4,1),
+            //    new Point(3,1)
+
+        };
+        void Drawbu(int x ,int y)
+        {
+            int count=0;
+            foreach(Button b in bu)
+            {
+                b.BackColor = Color.White;
+                //b.Text = Qtable[count].ToString();
+                count++;
+            }
+            foreach (var a in trap)
+                bu[a.X, a.Y].BackColor = Color.Black;
+            foreach (var a in treasure)
+                bu[a.X, a.Y].BackColor = Color.Green;
+            bu[x, y].BackColor = Color.Red;
+        }
+        bool stop = false;
+        private void button3_Click(object sender, EventArgs e)
+        {
+            stop = false;
+            double alpha = 0.5;
+            double beta = 0.5;
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Time");
+            dataGridView1.DataSource = dt;
+            Task.Run(() =>
+            {
+                int x = 0;
+                int y = 0;
+                int r=0;
+                Random rd = new Random();
+                int count = 0;
+                while (!stop)
+                {
+                    List<search_value> candown = new List<search_value>();
+                    candown = candown_point(x,y);
+                    foreach (var a in candown)
+                    {
+                        List<search_value> candownM = new List<search_value>();
+                        candownM = candown_point(a.point.X, a.point.Y);
+                        double max_Q = candownM.Max(z => z.value);
+                        if (trap.FindAll(z => z.X == a.point.X && z.Y == a.point.Y).Count != 0)
+                        {
+                            r = -1;
+                            beta = 0;
+                        }
+                        else if (treasure.FindAll(z => z.X == a.point.X && z.Y == a.point.Y).Count != 0)
+                        {
+                            r = 1;
+                            beta = 0;
+                        }
+                        else
+                        {
+                            beta = 0.8;
+                            r = 0;
+                        }
+                        Qtable[x,y,a.s] = a.value + alpha * (r + (beta * max_Q) - a.value);
+                    }
+                    var candow_g = candown.GroupBy(z=>z.value).Select(z=>z.ToList()).ToList().OrderBy(z=>z.Max(zz=>-zz.value)).ToList();
+                    int rdn = rd.Next(0,candow_g[0].Count);
+                    x = candow_g[0][rdn].point.X;
+                    y = candow_g[0][rdn].point.Y;
+                    Drawbu(x,y);
+                    count++;
+                    if(treasure.FindAll(z => z.X == x && z.Y == y).Count != 0 || trap.FindAll(z => z.X == x && z.Y == y).Count != 0)
+                    {
+                        Thread.Sleep(20);
+                        x = 0;
+                        y = 0;
+                        dt.Rows.Add(count);
+                        if (dt.Rows.Count > 5) dt.Rows.RemoveAt(0);
+                        count = 0;
+                        Drawbu(x, y);
+                       
+                    }
+                    Thread.Sleep(strp_speed);
+                }
+            });
+        }
+        public List<search_value> candown_point(int x , int y)
+        {
+            List<search_value> candown = new List<search_value>();
+            if (chech_wall(x - 1, y)) candown.Add(new search_value() { point = new Point(x - 1, y), value = Qtable[x,y,0] ,s=0});
+            if (chech_wall(x + 1, y)) candown.Add(new search_value() { point = new Point(x+1, y), value = Qtable[x,y,1] ,s=1});
+            if (chech_wall(x, y - 1)) candown.Add(new search_value() { point = new Point(x, y-1), value = Qtable[x,y,2],s=2 });
+            if (chech_wall(x, y + 1)) candown.Add(new search_value() { point = new Point(x, y+1), value = Qtable[x, y, 3],s=3 });
+            return candown;
+        }
+        public bool chech_wall(int x ,int y)
+        {
+            if (x < 0 || y < 0 || x == 5 || y == 5) return false;
+            else return true;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            addbutton();
+            Drawbu(0,0);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            stop = stop ==  true?false:true;
+        }
+        int strp_speed = 10;
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            label1.Text = trackBar1.Value.ToString();
+            strp_speed = trackBar1.Value;
+        }
+    }
+}
+public class search_value
+{
+    public Point point { get; set; }
+    public int s { get; set; }
+    public double value { get; set; }
+}
